0b7fae7ae7e860c1e024fcb46b1c78546bd4f395
LAyaJnamiJ <layajnamij@gmail.com>
2022-04-29 15:14:16 +0800

    Deleted catergory and restaurant collection types, added value collection type.

--- src/api/value/controllers/value.js
+++ src/api/value/controllers/value.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  restaurant controller
+ *  value controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::restaurant.restaurant');
+module.exports = createCoreController('api::value.value');
